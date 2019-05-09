344863f52026c91348fd5b4c70595a6d82298881
Bill Gestrich <bill@foreflight.com>
2019-05-09 18:15:50 -0400

    Add note

--- FFGitClientTool/main.swift
+++ FFGitClientTool/main.swift
@@ -9,12 +9,32 @@
 import Foundation
 import SwiftGit2
 
+/*
+ Trying to set SSH to work, I did:
+ SwiftGit2 (master) $ echo 'export PATH="/usr/local/opt/curl-openssl/bin:$PATH"' >> ~/.bash_profile
+ SwiftGit2 (master) $ export LDFLAGS="-L/usr/local/opt/openldap/lib"
+ SwiftGit2 (master) $ export CPPFLAGS="-I/usr/local/opt/openldap/include"
+ 
+ */
+
 let URL: NSURL = NSURL(fileURLWithPath: "/Users/bill/dev/ios")
 print("\(FileManager.default.fileExists(atPath: URL.path!))")
 let repoResult = Repository.at(URL as URL)
 
 print("Is Valid \(Repository.isValid(url: URL as URL))")
 
-//        let repo = try! repoResult.get()
+let repo = try! repoResult.get()
 
+let remoteResult = repo.remote(named: "origin")
+
+let remote = try! remoteResult.get()
+
+let fetchresult = repo.fetch(remote)
+let res = fetchresult.map { "The random number is: \($0)." }
+print(res)
+//if let result = try? fetchresult.get() {
+//    print("result = \(result)")
+//} else {
+//    print("Error \(fetchresult.error)")
+//}
 
